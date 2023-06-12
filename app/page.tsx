6676c27453abd1ffb740f91cf474b7fa47ac69bb
Daisy <daisy.le0203@gmail.com>
2023-06-11 21:14:31 -0500

    implement the underline indicator

--- app/page.tsx
+++ app/page.tsx
@@ -154,8 +154,21 @@ export default function Home() {
           <span>03</span> Interactive elements
         </h2>
         {/* navigation */}
-
-        <div></div>
+        <div>
+          <nav>
+            <ul className="primary-navigation underline-indicators flex">
+              <li className="active">
+                <a className="uppercase text-white letter-spacing-2" href="#"><span>01</span>Active</a>
+              </li>
+              <li>
+                <a className="uppercase text-white letter-spacing-2" href="#"><span>01</span>Hovered</a>
+              </li>
+              <li>
+                <a className="uppercase text-white letter-spacing-2" href="#"><span>01</span>Idle</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
 
         <div className="flex">
           <div style={{ marginTop: "5rem" }}>
